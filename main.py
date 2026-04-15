037b3b1cb17468e7aaa7a2044788124b0295aa38
nilessie <257730676+nilessie@users.noreply.github.com>
2026-04-15 13:32:51 +0200

    Added num_games, Player class, and result logging

--- main.py
+++ main.py
@@ -1,47 +1,82 @@
 import random
 
-# RULES # 
-## Rock beats Scissors
-## Scissors beats Paper
-## Paper beats Rock
-## If both players create the same formation, then the game is a tie or draw.
 
+class Player:
+    def __init__(self, name):
+        self.player_name = name
+        self.choice = None
+        self.wins = 0
+        self.loses = 0
+        self.ties = 0
 
 class SPR:
     def __init__(self, id, name, symbol):
         self.id = id
         self.name = name
         self.symbol = symbol
-        self.wins = 0
-        self.loses = 0
-        self.ties = 0
-        #winning_conditions = None
-    
+
 
 def main():
-    #num_games = input("Best of how many games: ")
-    while True:
-        player_choice = input("Choose Scissors [S], Paper [P], or Rock [R]: ")
-        computer_choice = random.randint(0, 2)
+    num_games = 0
 
-        player1 = class_assigner(player_choice)
-        player2 = class_assigner(computer_choice)
+    while True:
+        try:
+            num_games = int(input("Enter a number of how many games to play: "))
+            if num_games <= 0:
+                print(f"Input must be greater then 0")
+            else:
+                break
+        except ValueError:
+            print("Input must be a number")
+
+    num_games_copy = num_games
+    round_num = 1
+    accepted_choices = ["s", "p", "r"]
+    player1 = Player("Player 1")
+    player2 = Player("Player 2")
+    
+    # MAIN GAME LOOP
+    while num_games != 0:
+        # Gets the choice of player and computer
+        print(f"\nRound: {round_num}")
+        player_choice = str(input("\nChoose one:\nScissors [S]\nPaper [P]\nRock [R]\n\n# "))
+        computer_choice = accepted_choices[random.randint(0, 2)]
+        
+        # Makes sure the player input is valid.
+        try:
+            formatted_player_choice = player_choice[0].lower()
+            if formatted_player_choice.isalpha() != True or formatted_player_choice not in accepted_choices:
+                print(f"Input '{formatted_player_choice}' not valid. Valid inputs are S, P, or R")
+                continue
+        except Exception as e:
+            print(f"Input '{player_choice}' not valid. Valid inputs are S, P, or R")
+            continue
+
+        player1.choice = class_assigner(formatted_player_choice)
+        player2.choice = class_assigner(computer_choice)
 
         game_verdict = winner_validator(player1, player2)
         if game_verdict is None:
-            print(f"{player1.name} and {player2.name}. It's a TIE! ")
-        elif game_verdict is player1:
-            print(f"{player1.name} beats {player2.name}. Player 1 wins!")
+            print(f"{player1.choice.name} and {player2.choice.name}. It's a TIE! ")
+            write_round_results(game_verdict, round_num)
+        elif game_verdict.player_name == player1.player_name:
+            write_round_results(game_verdict, round_num)
+            print(f"{player1.choice.name} beats {player2.choice.name}. Player 1 wins!")
         else:
-            print(f"{player2.name} beats {player1.name}. Player 2 wins!")
+            write_round_results(game_verdict, round_num)
+            print(f"{player2.choice.name} beats {player1.choice.name}. Player 2 wins!")
 
+        num_games -= 1
+        round_num += 1
+
+    write_game_results(player1, player2, num_games_copy)
 
 
 # Assign a object to the player choices
 def class_assigner(choice):
-    if choice == 0:
+    if choice == "s":
         choice = SPR(id= 0, name="Scissors", symbol="✂️")
-    elif choice == 1:
+    elif choice == "p":
         choice = SPR(id= 1, name="Paper", symbol="📄")
     else:
         choice = SPR(id= 2, name="Rock", symbol="🪨")
@@ -49,47 +84,49 @@ def class_assigner(choice):
     return choice
 
 
-# Compare the choices and declare a winner
+# Compares the choices made and returns the results
 def winner_validator(player1, player2):
-    winner = None
     # Player 1 wins
     if (
-        player1.id == 0 and player2.id == 1 or
-        player1.id == 1 and player2.id == 2 or
-        player1.id == 2 and player2.id == 0
+        player1.choice.id == 0 and player2.choice.id == 1 or
+        player1.choice.id == 1 and player2.choice.id == 2 or
+        player1.choice.id == 2 and player2.choice.id == 0
         ):
-        winner = player1
         player1.wins += 1
         player2.loses += 1
+        return player1
     # Player 2 wins
     elif (
-        player2.id == 0 and player1.id == 1 or
-        player2.id == 1 and player1.id == 2 or
-        player2.id == 2 and player1.id == 0
+        player2.choice.id == 0 and player1.choice.id == 1 or
+        player2.choice.id == 1 and player1.choice.id == 2 or
+        player2.choice.id == 2 and player1.choice.id == 0
         ):
-        winner = player2
         player2.wins += 1
         player1.loses += 1
+        return player2
     # Tie
     else:
         player1.ties += 1
         player2.ties += 1
-    
-    return winner
-    
-    
-    
+        return None
 
+
+# Writes the results for the round
+def write_round_results(game_verdict, round_num):
+    if game_verdict == None:
+        with open("results.txt", "a") as f:
+            f.write(f"Round {round_num}: Tied\n")
+    else:
+        with open("results.txt", "a") as f:
+            f.write(f"Round {round_num} winner: {game_verdict.player_name}\n")
+
+
+# Writes the results of the entire game
+def write_game_results(player1, player2, num_games):
+    with open("results.txt", "a") as f:
+        f.write(f"\n----- GAME RESULTS -----\nPlayer 1 Stats:\nWins: {player1.wins}\nLoses: {player1.loses}\nTies: {player1.ties}\n------------------------\n\n")
+    print(f"\n----- GAME RESULTS -----\nPlayer 1 Stats:\nWins: {player1.wins}\nLoses: {player1.loses}\nTies: {player1.ties}\n------------------------\n\n")
 
 
 main()
-
-
-
-
-
-    
-
-
-
 
